071e2cdadda6305dfc01cfbc96d4cfc7376d2413
lahgit <jesse.lahti@helsinki.fi>
2024-11-15 14:27:03 +0200

    Added schema

---
+++ src/schema.sql
@@ -0,0 +1,39 @@
+CREATE TABLE "referneces" (
+  "id" SERIAL PRIMARY KEY,
+  "name" TEXT UNIQUE
+);
+
+CREATE TABLE "inproceedings" (
+  "id" SERIAL PRIMARY KEY,
+  "author" TEXT,
+  "title" TEXT,
+  "year" integer,
+  "booktitle" TEXT,
+  "reference_id" INTEGER
+);
+
+CREATE TABLE "books" (
+  "id" SERIAL PRIMARY KEY,
+  "author" TEXT,
+  "title" TEXT,
+  "year" integer,
+  "publisher" TEXT,
+  "reference_id" INTEGER
+);
+
+CREATE TABLE "articles" (
+  "id" SERIAL PRIMARY KEY,
+  "author" TEXT,
+  "title" TEXT,
+  "journal" text,
+  "year" integer,
+  "volume" integer,
+  "pages" TEXT,
+  "reference_id" INTEGER
+);
+
+ALTER TABLE "books" ADD FOREIGN KEY ("reference_id") REFERENCES "referneces" ("id") ON DELETE CASCADE;
+
+ALTER TABLE "articles" ADD FOREIGN KEY ("reference_id") REFERENCES "referneces" ("id") ON DELETE CASCADE;
+
+ALTER TABLE "inproceedings" ADD FOREIGN KEY ("reference_id") REFERENCES "referneces" ("id") ON DELETE CASCADE;
