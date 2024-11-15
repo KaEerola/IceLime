dac2befbb2da018f4b30f1a533dba20fe599bab2
lahgit <jesse.lahti@helsinki.fi>
2024-11-15 15:31:41 +0200

    db_helper done

--- src/db_helper.py
+++ src/db_helper.py
@@ -43,6 +43,86 @@ def setup_db():
   db.session.execute(sql)
   db.session.commit()
 
+  #inproceedings
+
+
+
+  if table_exists("inproceedings"):
+    print(f"Table inproceedings exists, dropping")
+    sql = text(f"DROP TABLE inproceedings")
+    db.session.execute(sql)
+    db.session.commit()
+
+
+  sql2 = text(
+    """
+    CREATE TABLE inproceedings (
+        id SERIAL PRIMARY KEY,
+        author TEXT,
+        title TEXT,
+        year INTEGER,
+        booktitle TEXT,
+        reference_id INTEGER
+    )
+    """
+  )
+  
+  db.session.execute(sql2)
+  db.session.commit()
+
+  #books
+
+  if table_exists("books"):
+    print(f"Table books exists, dropping")
+    sql = text(f"DROP TABLE books")
+    db.session.execute(sql)
+    db.session.commit()
+
+  sql3 = text(
+    """
+    CREATE TABLE "books" (
+    "id" SERIAL PRIMARY KEY,
+    "author" TEXT,
+    "title" TEXT,
+    "year" integer,
+    "publisher" TEXT,
+    "reference_id" INTEGER
+      )
+    """
+  )
+
+  db.session.execute(sql3)
+  db.session.commit()
+
+  #articles
+
+  if table_exists("articles"):
+    print(f"Table articles exists, dropping")
+    sql = text(f"DROP TABLE articles")
+    db.session.execute(sql)
+    db.session.commit()
+
+
+  sql4 = text(
+    """
+    CREATE TABLE "articles" (
+    "id" SERIAL PRIMARY KEY,
+    "author" TEXT,
+    "title" TEXT,
+    "journal" text,
+    "year" integer,
+    "volume" integer,
+    "pages" TEXT,
+    "reference_id" INTEGER
+    )
+    """
+  )
+
+
+  db.session.execute(sql4)
+  db.session.commit()
+
+
 if __name__ == "__main__":
     with app.app_context():
       setup_db()
