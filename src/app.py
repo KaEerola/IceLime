56ba95d7d279a7254bb9d6cd82c7792fa77759a4
ratilmii <ratilmii@gmail.com>
2024-11-15 16:11:21 +0200

    add route to view_references.html

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import redirect, render_template, request, jsonify, flash
 from db_helper import reset_db
 from repositories.todo_repository import get_todos, create_todo, set_done
+from repositories.book_repository import add_book, get_books
 from config import app, test_env
 from util import validate_todo
 
@@ -16,7 +17,7 @@ def add_reference():
 
 @app.route("/view_references")
 def view_references():
-
+    books = get_books
     return render_template("view_references.html")
     
 
