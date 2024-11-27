cb57fbeee82bd95d260581b958f65509402cae91
neononoen <roosa.nenonen@outlook.com>
2024-11-27 12:28:57 +0200

    update to the validation, can't input a negative year

--- src/util.py
+++ src/util.py
@@ -4,6 +4,8 @@ class UserInputError(Exception):
 def validate_book(content):
     if int(content[3]) > 2024:
         raise UserInputError("This year hasn't come yet")
+    if int(content[3]) < 0:
+        raise UserInputError("You can't input a negative year")
 
     for i in range(4):
         if content[i] == "":
