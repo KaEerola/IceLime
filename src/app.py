c0f6a690eb1f22ec92735c11324018c56a1f5a01
ratilmii <ratilmii@gmail.com>
2024-12-02 17:30:53 +0200

    fix typo in app.py

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from flask import redirect, render_template, request, jsonify, flash, send_file
 from db_helper import reset_db
 from repositories.book_repository import add_user_book, get_books, get_book_by_id
 from repositories.book_repository import update_book, remove_book
-from repositories.article_repository import add_user_article, get_articles,
+from repositories.article_repository import add_user_article, get_articles
 from repositories.article_repository import remove_article
 from repositories.inproceeding_repository import add_user_inproceeding, get_inproceedings
 from repositories.inproceeding_repository import remove_inproceeding
